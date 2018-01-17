c2e8da40610ddeca689e7a945b6af0fd62c1e7af
Dumitru Cristina-Nicoleta <33783647+meow123321@users.noreply.github.com>
2018-01-18 00:08:03 +0200

    Delete Lab1 S3 3.py

--- Lab1 S3 3.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> num='Dumitru'
->>> pnum='Cristina'
->>> print(num *10)
-DumitruDumitruDumitruDumitruDumitruDumitruDumitruDumitruDumitruDumitru
->>> print(pnum *10)
-CristinaCristinaCristinaCristinaCristinaCristinaCristinaCristinaCristinaCristina
->>> 
