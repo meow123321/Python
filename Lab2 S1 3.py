fd8bf9c69e1d1e6f23904d08b37a9f535463e5db
Dumitru Cristina-Nicoleta <33783647+meow123321@users.noreply.github.com>
2018-01-17 14:35:57 +0200

    Add files via upload

---
+++ Lab2 S1 3.py
@@ -0,0 +1,8 @@
+Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> v=[12,23,34,45,56,67,78,89,90]
+>>> print('Minimul este ', min(v))
+Minimul este  12
+>>> print('Maximul este ', max(v))
+Maximul este  90
+>>> 
---
+++ Lab3 S1 2.py
@@ -0,0 +1,13 @@
+Python 3.7.0a4 (v3.7.0a4:07c9d85, Jan  9 2018, 07:07:02) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=[12, 23, 34, 45, 56, 67]
+>>> search=12
+>>> i=0
+>>> while i<6:
+	if a[i]==search:
+		print('Indexul numarului cautat este: ', i)
+		break
+
+	
+Indexul numarului cautat este:  0
+>>> 
